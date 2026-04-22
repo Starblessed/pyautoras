e0c293750773d395f6ebcc1a3e278394af379a7c
Starblessed <danmauricio001@gmail.com>
2026-04-22 07:55:46 -0300

    feat: win32com ras controller wrapper class

---
+++ src/ras/RASController.py
@@ -0,0 +1,50 @@
+from win32com import client
+import pythoncom
+from exceptions import InvalidVersionException
+
+class RasController:
+    
+    def __init__(self, version: str='7.0') -> None:
+        self._version = version
+        self._controller = self._get_w32_ras_controller(version=version)
+        
+        
+    def _get_ras_dispatch_string(self, version: str) -> str:
+        """Gets the RAS Dispatch string from the specified RAS version.
+
+        Args:
+            version (float): RAS version to use.
+
+        Returns:
+            str: RAS Dispatch string.
+        """
+        
+        return f"RAS{version.replace(".", "")}.HECRASController"
+    
+    def _get_w32_ras_controller(self, version: str) -> client.CDispatch:
+        """Gets the win32com RAS Controller Object.
+
+        Args:
+            version (float): RAS version to use.
+
+        Returns:
+            win32com.client.CDispatch: win32com RAS Controller Object.
+        """
+        
+        try:
+            return client.Dispatch(self._get_ras_dispatch_string(version)) # type: ignore
+            
+        except pythoncom.com_error:
+            raise(InvalidVersionException(f"Invalid RAS version {version}"))
+        
+    def __repr__(self):
+        return f"RAS Controller for HEC-RAS v{self._version}\nMethods: {dir(self)}"
+
+if __name__ == "__main__":
+    
+    # Creates an instance of the RasController class
+    rc = RasController(version="7.0")
+    
+    # Shows the object's string representation
+    print(rc)
+    
---
+++ src/ras/exceptions.py
@@ -0,0 +1,3 @@
+class InvalidVersionException(Exception):
+    """Exception raised when the provided RAS version is not valid."""
+    pass
