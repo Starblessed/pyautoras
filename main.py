0483112d70342797c654ae514a50431319756fcc
Starblessed <danmauricio001@gmail.com>
2026-04-22 07:23:20 -0300

    add: ras dispatch string getter

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from win32com import client
+
+def get_ras_dispatch_string(version: str) -> str:
+    """Gets the RAS Dispatch string from the specified RAS version.
+
+    Args:
+        version (float): RAS version to use.
+
+    Returns:
+        str: RAS Dispatch string.
+    """
+    
+    return f"RAS{version.replace(".", "")}.HECRASController"
+
 def main():
-    print("Hello from pyautoras!")
+    ras_dstr = get_ras_dispatch_string("7.0")
+    
+    print(ras_dstr)
 
 
 if __name__ == "__main__":
