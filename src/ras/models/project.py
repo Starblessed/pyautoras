2f9c87df55044631c52126fffc0cc5a26f1321c1
Starblessed <danmauricio001@gmail.com>
2026-05-03 18:54:03 -0300

    fix: ProjectManager requiring path on initialization

--- src/ras/models/project.py
+++ src/ras/models/project.py
@@ -7,6 +7,7 @@ from os.path import splitext
 from datetime import datetime, timedelta
 
 
+
 class ProjectConfig(ConfigFile):
     def __init__(self, filepath: str) -> None:
         if not os.path.splitext(filepath)[1].startswith(".prj"):
@@ -16,12 +17,17 @@ class ProjectConfig(ConfigFile):
 
 
 class ProjectManager:
-    def __init__(self, project_config: ProjectConfig):
-        self.config = project_config
+    def __init__(self, project_config: ProjectConfig | None=None):
+        self.current_project = project_config
+        
+    def change_project(self, project_config: ProjectConfig):
+        self.current_project = project_config
 
     def add_unsteady_file(self, filepath: str) -> None:
+        assert self.current_project is not None, "Can't perform operation because there is no selected project"
+        
         unsteady_lines = []
-        for i, l in enumerate(self.config._lines):
+        for i, l in enumerate(self.current_project._lines):
             if "Unsteady File=" in l:
                 unsteady_lines.append(i)
         
@@ -31,14 +37,16 @@ class ProjectManager:
         new_unsteady_id = splitext(filepath)[1][1:]
         
         new_unsteady_line = f"Unsteady File={new_unsteady_id}\n"
-        if not new_unsteady_line in self.config._lines:
-            self.config._lines[start:end] = self.config._lines[start:end] + [new_unsteady_line]
+        if not new_unsteady_line in self.current_project._lines:
+            self.current_project._lines[start:end] = self.current_project._lines[start:end] + [new_unsteady_line]
         
-        self.config.save_changes()
+        self.current_project.save_changes()
         
     def add_plan_file(self, filepath: str) -> None:
+        assert self.current_project is not None, "Can't perform operation because there is no selected project"
+        
         plan_lines = []
-        for i, l in enumerate(self.config._lines):
+        for i, l in enumerate(self.current_project._lines):
             if "Plan File=" in l:
                 plan_lines.append(i)
         
@@ -47,13 +55,15 @@ class ProjectManager:
         
         new_plan_id = splitext(filepath)[1][1:]
         new_plan_line = f"Plan File={new_plan_id}\n"
-        if not new_plan_line in self.config._lines:
-            self.config._lines[start:end] = self.config._lines[start:end] + [new_plan_line]
+        if not new_plan_line in self.current_project._lines:
+            self.current_project._lines[start:end] = self.current_project._lines[start:end] + [new_plan_line]
         
-        self.config.save_changes()
+        self.current_project.save_changes()
         
     def set_current_plan(self, filepath: str) -> None:
+        assert self.current_project is not None, "Can't perform operation because there is no selected project"
+        
         plan_id = splitext(filepath)[1][1:]
         
-        self.config._set_attribute("Current Plan", plan_id)
-        self.config.save_changes()
+        self.current_project._set_attribute("Current Plan", plan_id)
+        self.current_project.save_changes()
